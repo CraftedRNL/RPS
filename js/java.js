38058489fcf772c823672bf5d9fea716a259bf79
Ryan <126Rliu@frhsd.com>
2024-12-04 13:58:12 -0500

    started js

---
+++ js/java.js
@@ -0,0 +1,21 @@
+let rng = Math.floor((Math.random()*3)+1);
+var cpu = '';
+if(rng == 1){
+    cpu = 'rock'
+}else if(rng == 2){
+    cpu = 'paper'
+}else {
+    cpu = 'scissor'
+}
+console.log(cpu)
+
+
+let cc = '';
+
+document.querySelectorAll("img").forEach((img) => {
+   
+    });
+    document.querySelectorAll("img").addEventListener("click", function {
+        console.log(pinput);
+      });
+    
