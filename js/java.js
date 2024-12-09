2ec6349be037193d295029d654737e6c8caf492c
Ryan <126Rliu@frhsd.com>
2024-12-09 12:44:27 -0500

    Will improve design. Pretty sure js is working.

--- js/java.js
+++ js/java.js
@@ -1,16 +1,62 @@
-let rng = Math.floor((Math.random()*3)+1);
+let rng;
 var cpu = '';
-if(rng == 1){
-    cpu = 'rock'
-}else if(rng == 2){
-    cpu = 'paper'
-}else {
-    cpu = 'scissor'
+function computer() {
+    rng = Math.floor((Math.random()*3)+1);
+    if(rng == 1){
+        cpu = 'rock'
+    }else if(rng == 2){
+        cpu = 'paper'
+    }else {
+        cpu = 'scissor'
+    }
 }
 console.log(cpu)
 
+cpu;
+let pc = '';
 
-let cc = '';
+
+document.getElementById('rock').addEventListener("click", function rock() {
+    pc = 'rock'
+    if(cpu == 'scissor' && pc =='rock'){
+        alert('You Win')
+    }
+    else if(cpu == pc){
+        alert('You Tie')
+    }else {
+        alert('You Lose')
+    }
+    computer();
+});
+document.getElementById('paper').addEventListener("click", function() {
+    
+    pc = 'paper'
+    if(cpu == 'rock' && pc == 'paper'){
+        alert('You Win')
+    }else if(cpu == pc){
+        alert('You Tie')
+    }else {
+        alert('You Lose')
+    }
+    computer();
+});
+
+document.getElementById('scissor').addEventListener("click", function() {
+    pc = 'scissor'
+    if(cpu == 'paper' && pc == 'scissor'){
+        alert('You Win')
+    }else if(cpu == pc){
+        alert('You Tie')
+    }else {
+        alert('You Lose')
+    }
+    computer();
+});
 
 
+
+
+
+    
+    
  
